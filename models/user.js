2979218536d6acfa62661e539a6e72897f686386
Magda-J <magdalena.jakonska@gmail.com>
2024-02-09 16:53:42 +0000

    Post function, edit and delete update

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
-  token: String
+  token: String,
+  // testing
+  // userEvent: [{
+  //   refId: { type: mongoose.Schema.Types.ObjectId, ref: "events" },
+    
+  // }]
+  // testing
 })
 
 module.exports.User = mongoose.model('User', userSchema)
